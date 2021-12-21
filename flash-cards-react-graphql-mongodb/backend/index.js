f76d5a74cb75bf784ab4995d14f3aa82084a0446
nnikitins <nikita.nikitin@jeanedwards.com>
2021-12-21 21:43:18 +0100

    backend added

--- flash-cards-react-graphql-mongodb/backend/index.js
+++ flash-cards-react-graphql-mongodb/backend/index.js
@@ -1,6 +1,26 @@
+import express from 'express';
 import mongoose from 'mongoose';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+const port = 4000;
 
 mongoose.connect('mongodb://localhost/flashcards_db', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+  res.send('It works!');
+});
+
+server.start().then(res => {
+  server.applyMiddleware({ app, path: '/' });
+  app.listen({ port }, () => 
+    console.log(`Gateway API running at port: ${port}`)
+  );  
 });
