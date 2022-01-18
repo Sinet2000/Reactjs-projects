ce5aa8b431aeac73937ff8aac1e940d74b94e005
nnikitins <nikita.nikitin@jeanedwards.com>
2022-01-18 21:42:51 +0100

    added eslint, prettierc, tests, bootstrap, sass, DockerFile

---
+++ react-ts-sass-docker-boilerplate/front-end/src/index.tsx
@@ -0,0 +1,6 @@
+import './css/bootstrap.scss';
+import * as React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+const rootElement = document.getElementById('root');
+render(<App />, rootElement);
